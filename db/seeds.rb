3cac8c8e3e5cb1f5df1fac693f354edd4cab51a4
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-21 18:04:30 +0100

    changed some elements of the model + modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,39 +14,54 @@ require "open-uri"
 # liste des départements:
 # https://collectionapi.metmuseum.org/public/collection/v1/departments
 
+louvre_admin = User.new(first_name: "Louvre", last_name: "admin", email: "louvre@admin.com", password: "louvre")
+louvre_admin.save
+
 all_artworks_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11|9"
 artworks_serialized = URI.open(all_artworks_url).read
 all_artworks = JSON.parse(artworks_serialized)
-artworks_ids = all_artworks["objectIDs"].sample(100)
-
+artworks_ids = all_artworks["objectIDs"].sample(10)
 
 artworks_ids.each do |artwork_id|
   url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/#{artwork_id}"
   artwork_serialized = URI.open(url).read
   artwork = JSON.parse(artwork_serialized)
-  if artwork["isHighlight"] == true && artwork["primaryImage"] != "" && artwork["dimensions"] != ""
+  if artwork["primaryImage"] != "" && artwork["dimensions"] != ""
     title = artwork["title"]
+    p title
     creator_name = artwork["artistDisplayName"]
     creation_date = artwork["objectDate"]
     dimensions = artwork["dimensions"]
     primary_image = artwork['primaryImage']
-    type = artwork["objectName"]
+    medium = artwork["objectName"]
     description = "this #{artwork["medium"]} from #{creator_name} was made in #{artwork["country"]}.It perfectly
     represents the #{artwork["period"]} period. It was acquired by the Metropolitan museum in #{artwork["accessionYear"]}"
-    Artwork.create(title: title, creator: creator_name, description: description,
-    dimensions: dimensions, creation_date: creation_date, type: type, price_per_day: rand[2000..20_000])
-    Image.create(url: primary_image)
-    if artwork["additionalImages"] != []
-      Image.create(url: artwork['additionalImages'][0]) unless artwork['additionalImages'][0].nil?
-      Image.create(url: artwork['additionalImages'][1]) unless artwork['additionalImages'][1].nil?
-      Image.create(url: artwork['additionalImages'][1]) unless artwork['additionalImages'][2].nil?
-      Image.create(url: artwork['additionalImages'][1]) unless artwork['additionalImages'][3].nil?
-    end
+    new_artwork = Artwork.new(
+      title: title,
+      creator: creator_name,
+      description: description,
+      dimensions: dimensions,
+      creation_date: creation_date,
+      medium: medium,
+      price_per_day: rand(2000..20_000)
+    )
+    new_artwork.user = louvre_admin
+    new_artwork.save!
+    p new_artwork
+    # first_image = Image.new(url: primary_image)
+    # first_image.artwork = new_artwork
+    # first_image.save
+    # if artwork["additionalImages"] != []
+    #   artwork[0...4].each do |image_url|
+    #     new_image = Image.new(url: image_url) unless image_url.nil?
+    #     new_image.artwork = new_artwork
+    #     new_image.save
+    #   end
+    # end
   end
 end
 
-# CHANGER WIDHT AND HEIGHT TO DIMENSIONS (STRING)
-
+# artwork["isHighlight"] == true &&
 
 # t.string "title"
 # t.string "creator"
