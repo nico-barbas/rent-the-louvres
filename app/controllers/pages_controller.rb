f0def04fdfb648e905e0cf786db7b2f9f500ca20
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-22 17:02:58 +0100

    removed login requirement to artworks index and show pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
+  skip_before_action :authenticate_user!, only: [:home, :artworks, :artwork]
   def home
   end
 end
