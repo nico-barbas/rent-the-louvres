48a5f1c6e0bfe596998a54151904607aa3faa77f
nico-bb <nicolas.barbas@protonmail.com>
2022-11-22 15:19:00 +0100

    added artwork show page + artworks authorization

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,11 @@
 class ArtworksController < ApplicationController
   def index
-    @artworks = Artwork.all[0...50]
+    @artworks = policy_scope(Artwork)
+    @artworks = @artworks[0...50]
+  end
+
+  def show
+    @artwork = Artwork.find(params[:id])
+    authorize @artwork
   end
 end
