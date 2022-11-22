54992ad0e572c023e6bdd9902e0933891c444c2e
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-22 11:22:23 +0100

    pundit basic set up done

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,8 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
