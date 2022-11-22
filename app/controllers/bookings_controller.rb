e79c70ac9db5d73ed60ebb51a86d6a4f23014f75
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-22 15:04:46 +0100

    dashboard page - back end code done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+
+  def index
+    @current_user = current_user
+    @user_bookings = @current_user.bookings
+    @user_bookings = policy_scope(Booking)
+  end
 end
