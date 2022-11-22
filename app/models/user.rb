f6e5bd9e6a2d0a3c954185c51c48bbc28118a5cb
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-22 15:07:29 +0100

    dashboard page - back end done (+typo corrected)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :bookings
-  has_many :patings
+  has_many :artworks
   validates :first_name, presence: true
   validates :first_name, length: { minimum: 2 }
   validates :last_name, presence: true
