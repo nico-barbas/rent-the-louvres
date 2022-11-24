0a099115a8f08c4f9222d940a63221e796cfb4cd
nico-bb <nicolas.barbas@protonmail.com>
2022-11-24 15:02:55 +0100

    added category filters for search bar

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,18 +1,20 @@
+MEDIUMS = ["Painting", "Print", "Drawing", "Sculpture"]
+
 class ArtworksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  
   def index
     @artworks = policy_scope(Artwork)
-    @artworks.each do |artwork|
-      if artwork.images.length > 1
-        puts artwork.creator
-      end
-    end
     if params[:query].present?
       sql_query = "title ILIKE :query OR creator ILIKE :query"
       @artworks = Artwork.where(sql_query, query: "%#{params[:query]}%")
+      category = params.require("category").permit(:name)
+      @artworks = @artworks.where(medium: category[:name])
     else
       @artworks = @artworks[0...50]
     end
+
+    @categories = MEDIUMS
   end
 
   def show
