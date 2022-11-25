51a19c880e8f673ae8caabbcc1336526781e10fc
nico-bb <nicolas.barbas@protonmail.com>
2022-11-25 11:42:55 +0100

    made the scroll button dynamic

--- app/javascript/controllers/backup_controller.js
+++ app/javascript/controllers/backup_controller.js
@@ -2,7 +2,33 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="backup"
 export default class extends Controller {
+  static targets = ["scrollBtn"];
+
+  connect() {
+    this.scrollBtnTarget.hidden = true;
+  }
+
   scrollToTop() {
     window.scrollTo(0, 0);
+  }
+
+  controlBtn() {
+    const footerHeight = document.querySelector(".footer").offsetHeight;
+    const padding = 20;
+    const minScrollDistance = 650;
+    const maxScrollDistance =
+      document.body.scrollHeight - window.screen.height - footerHeight;
+
+    const maxPosY = footerHeight + padding;
+    if (window.scrollY >= minScrollDistance) {
+      this.scrollBtnTarget.hidden = false;
+      this.element.style.bottom = `${padding}px`;
+    } else {
+      this.scrollBtnTarget.hidden = true;
+    }
+
+    if (window.scrollY >= maxScrollDistance) {
+      this.element.style.bottom = `${maxPosY}px`;
+    }
   }
 }
